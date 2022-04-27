254adb846066734d7e3dedcd8f47098b43f27465
minhquach <minhquach8@gmail.com>
2022-04-27 18:35:45 +0700

    Update ReminderEdit

---
+++ tradingvision/build/precache-manifest.31043713404a9f3ea701c9d0487d4ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15022b31ed4099ea3a9a",
+    "url": "/static/css/main.df3ce081.chunk.css"
+  },
+  {
+    "revision": "15022b31ed4099ea3a9a",
+    "url": "/static/js/main.c1ab1f43.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9db8df383c13ae0bd5c4",
+    "url": "/static/css/2.99567302.chunk.css"
+  },
+  {
+    "revision": "9db8df383c13ae0bd5c4",
+    "url": "/static/js/2.e75db1af.chunk.js"
+  },
+  {
+    "revision": "af2cd87288b2d547791edd2e9567424b",
+    "url": "/index.html"
+  }
+];
